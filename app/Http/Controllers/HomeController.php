c24cda2f242ebf833fba73769069d58f50a388fa
Hanif Al Fathoni <alfatoni922@gmail.com>
2024-09-12 08:04:14 +0700

    Controllers

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+
+    public function index()
+    {
+        return view('welcome');
+    }
+
+    public function hello()
+    {
+        return 'hello world';
+    }
+
+    public function perkalian($angka)
+    {
+        return $angka * 2;
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,14 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [HomeController::class, 'index']);
 
-Route::get('/hello', function () {
-    return 'hello world';
-});
+Route::get('/hello',  [HomeController::class, 'hello']);
 
-Route::get('/perkalian/{angka}', function ($angka) {
-    return $angka * 2;
-});
+Route::get('/perkalian/{angka}', [HomeController::class, 'perkalian']);
